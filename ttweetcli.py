c069e38be646716354f7c868d38041ff4b7f92b0
dsaiontz <david.saiontz@gmail.com>
2020-04-03 20:12:16 -0400

    clean up tweet command

--- ttweetcli.py
+++ ttweetcli.py
@@ -69,29 +69,35 @@ def Main():
         if len(command) > 5 and command[0, 5].equals('tweet'):
             if len(command) < 7:
                 print('message length illegal, connection refused.')
+                continue
             messageAndHashTag = command[7:]
             if messageAndHashTag.find('"') == messageAndHashTag.rfind('"'):
                 print('hashtag illegal format, connection refused.')
-            else:
-                message = messageAndHashTag[0:messageAndHashTag.find('"')]
-                if len(message) > 150 or len(message) < 0:
-                    print('message length illegal, connection refused.')
+                continue
+            message = messageAndHashTag[0:messageAndHashTag.find('"')]
+            if len(message) > 150 or len(message) < 0:
+                print('message length illegal, connection refused.')
+                continue
+            hashTags = messageAndHashTag[:messageAndHashTag.find('"') + 2]
+            if len(hashTags) == 0 or hashTags.find('##') > -1 or hashTags.count('#') > 5 or hashTags.find('#ALL') > -1:
+                print('hashtag illegal format, connection refused.')
+                continue
+            lastIndex = 0
+            curHashTagsLeft = hashTags
+            shouldExitCommand = False
+            while lastIndex != hashTags.rfind('#'):
+                if curHashTagsLeft[1:].find('#') > 15:
+                    print('hashtag illegal format, connection refused.')
+                    shouldExitCommand = True
+                    break
                 else:
-                    hashTags = messageAndHashTag[:messageAndHashTag.find('"') + 2]
-                    if len(hashTags) == 0 or hashTags.find('##') > -1 or hashTags.count('#') > 5 or hashTags.find('#ALL') > -1:
-                        print('hashtag illegal format, connection refused.')
-                    else:
-                        lastIndex = 0
-                        curHashTagsLeft = hashTags
-                        while lastIndex != hashTags.rfind('#'):
-                            if curHashTagsLeft[1:].find('#') > 15:
-                                print('hashtag illegal format, connection refused.')
-                            else:
-                                curHashTagsLeft = curHashTagsLeft[curHashTagsLeft[1:].find('#'):]
-                        if len(curHashTagsLeft) > 15:
-                            print('hashtag illegal format, connection refused.')
-                        else:
-                            s.sendall(command.encode())
+                    curHashTagsLeft = curHashTagsLeft[curHashTagsLeft[1:].find('#'):]
+            if shouldExitCommand:
+                continue
+            if len(curHashTagsLeft) > 15:
+                print('hashtag illegal format, connection refused.')
+                continue
+            s.sendall(command.encode())
 
     #old, unaltered code begins here
 
